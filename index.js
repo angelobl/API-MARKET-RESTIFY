a5cc7d041255d5f9b945f43c2c0ba7ea60802555
MAYTESI <mayte16inga@gmail.com>
2019-10-28 13:57:33 -0500

    [add] endpoint GET /products, POST /products

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const restify = require('restify');
 const userRoutes = require('./main/routes/users/usersRoutes')
-
+const productRoutes = require('./main/routes/products/products.routes')
 const server = restify.createServer();
 const PORT = 4000;
 
@@ -9,6 +9,7 @@ server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
 userRoutes.applyRoutes(server);
+productRoutes.applyRoutes(server)
 
 server.listen(PORT,()=>{
     console.log(`listen on ${server.name}`, server.url);
---
+++ main/routes/products/products.routes.js
@@ -0,0 +1,41 @@
+const {Products} = require('../../Bd/db');
+const Route = require('restify-router').Router;
+
+const ProductsRoutes = new Route()
+
+ProductsRoutes.post('/products',(req,res,next)=>{
+  try{
+    //if(!name)
+    Users.push({...req.body})
+    res.end(JSON.stringify(Products)) 
+  }catch(e){
+      console.log(e)
+  }
+})
+
+ProductsRoutes.get('/products',(req,res,next)=>{
+  res.setHeader('Content-type', 'application/json')
+  res.writeHead(200)
+  res.end(JSON.stringify(Products))
+});
+
+ProductsRoutes.get('/products/:id',(req,res,next)=>{   
+  res.writeHead(201)
+  const product = Products.filter((elem)=>elem.id ===req.params.id)[0];
+  res.end(JSON.stringify(product)) 
+}); 
+
+ProductsRoutes.put('/products/:id', (req,res,next)=>{
+  try{
+    const product = Products.filter((elem)=>elem.id ===req.params.id)[0];
+    Products.push({
+      ...product,
+      ...req.body
+    })
+    res.end(JSON.stringify(product)) 
+  }catch(e){
+      console.log(e)
+  }
+})
+
+module.exports = ProductsRoutes;
